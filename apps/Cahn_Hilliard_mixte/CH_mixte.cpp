012894d84e5c12c44c9cc68d98f45671749dd44e
npaster <nicolas.pignet@orange.fr>
2017-06-05 21:10:36 +0200

    Début Chan Hilliard

---
+++ apps/Cahn_Hilliard_mixte/CH_mixte.cpp
@@ -0,0 +1,337 @@
+/*
+ *       /\
+ *      /__\       Matteo Cicuttin (C) 2016, 2017 - matteo.cicuttin@enpc.fr
+ *     /_\/_\      École Nationale des Ponts et Chaussées - CERMICS
+ *    /\    /\
+ *   /__\  /__\    DISK++, a template library for DIscontinuous SKeletal
+ *  /_\/_\/_\/_\   methods.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * If you use this code for scientific publications, you are required to
+ * cite it.
+ */
+
+#include <iostream>
+#include <regex>
+#include <unistd.h>
+#include <sstream>
+#include <iomanip>
+
+#include <map>
+
+#include "colormanip.h"
+
+#include "../../config.h"
+
+#ifdef HAVE_SOLVER_WRAPPERS
+    #include "agmg/agmg.hpp"
+#endif
+
+#include "loaders/loader.hpp"
+#include "hho/hho.hpp"
+#include "CHParameters.hpp"
+
+#include "timecounter.h"
+
+#define _USE_MATH_DEFINES
+#include <cmath>
+
+#include "CH_mixte_solver.hpp"
+
+struct run_params
+{
+    size_t  degree;
+    int     l;
+    bool    verbose;
+};
+
+
+
+template<template<typename, size_t , typename> class Mesh,
+         typename T, typename Storage>
+void
+run_CH_mixte_solver(const Mesh<T, 2, Storage>& msh, run_params& rp)
+{
+   typedef Mesh<T, 2, Storage> mesh_type;
+   typedef static_vector<T, 2> result_type;
+
+//    auto load = [](const point<T,2>& p) -> result_type {
+//       T fx = 2.*M_PI*M_PI*sin(M_PI*p.x())*sin(M_PI*p.y());
+//       T fy = 2.*M_PI*M_PI*cos(M_PI*p.x())*cos(M_PI*p.y());
+//       return result_type{fx,fy};
+//    };
+//
+//    auto solution = [](const point<T,2>& p) -> result_type {
+//       T fx = sin(M_PI*p.x())*sin(M_PI*p.y());
+//       T fy = cos(M_PI*p.x())*cos(M_PI*p.y());
+//
+//       return result_type{fx,fy};
+//    };
+
+
+   auto load = [](const point<T,2>& p) -> result_type {
+      T fx = -16.0 * p.x() * p.y() * p.y() -4.0 * p.x() * p.x() * p.x();
+      T fy = -16.0 * p.x() * p.x() * p.y() -4.0 * p.y() * p.y() * p.y();
+      return result_type{fx,fy};
+   };
+
+   auto solution = [](const point<T,2>& p) -> result_type {
+      T fx = p.x() * p.x() * p.y();
+      T fy = p.x() * p.y() * p.y();
+
+      return result_type{fx,fy};
+   };
+
+
+   CH_mixte_solver<Mesh, T, 2, Storage,  point<T, 2> > nl(msh, rp.degree);
+   nl.verbose(rp.verbose);
+
+   auto info_offline = nl.compute_offline();
+
+   if(nl.verbose()){
+      std::cout << "Off_line computations: " << info_offline.time_offline << " sec"  << '\n';
+   }
+
+   nl.compute_initial_state();
+
+   if(nl.verbose()){
+      std::cout << "Solve the problem: " << '\n';
+   }
+
+   const size_t n_time_step = 1;
+
+   solve_info solve_info = nl.compute(load, solution, n_time_step);
+
+   if(nl.verbose()){
+      std::cout << "Total time to solve the problem: " << solve_info.time_solver << " sec" << '\n';
+   }
+
+
+   if(nl.test_convergence()){
+
+        std::cout << "l2 error: " << nl.compute_l2_error(solution) << std::endl;
+
+        std::cout << "Post-processing: " << std::endl;
+        nl.plot_solution_at_gausspoint("sol_elas_2d.msh");
+        nl.plot_l2error_at_gausspoint("error_gp_2d_.msh", solution);
+   }
+
+   nl.saveMesh("msh2d.msh");
+   // nl.compute_deformed("deforme2d.msh");
+   // nl.compute_discontinuous_solution("depl2d.msh");
+}
+
+template<template<typename, size_t, typename> class Mesh,
+         typename T, typename Storage>
+void
+run_CH_mixte_solver(const Mesh<T, 3, Storage>& msh, run_params& rp)
+{
+   typedef Mesh<T, 3, Storage> mesh_type;
+   typedef static_vector<T, 3> result_type;
+
+    auto load = [](const point<T,3>& p) -> auto {
+      const T lambda =1.0;
+      T fx = M_PI*M_PI*(12*lambda*cos(2*M_PI*p.x())*sin(2*M_PI*p.y())*sin(2*M_PI*p.z())
+      + 8 * (3*cos(2*M_PI*p.x())-1)*sin(2*M_PI*p.y())*sin(2*M_PI*p.z())
+      - cos(M_PI*p.x())*sin(M_PI*(p.y()+p.z()))
+      + (1 + 3./(1+lambda))* sin(M_PI*p.x())*sin(M_PI*p.y())*sin(M_PI*p.z()) );
+
+      T fy = M_PI*M_PI*(12*lambda*cos(2*M_PI*p.y())*sin(2*M_PI*p.x())*sin(2*M_PI*p.z())
+      + 8 * (3*cos(2*M_PI*p.y())-1)*sin(2*M_PI*p.x())*sin(2*M_PI*p.z())
+      - cos(M_PI*p.y())*sin(M_PI*(p.x()+p.z())) +
+      (1 + 3./(1+lambda))* sin(M_PI*p.x())*sin(M_PI*p.y())*sin(M_PI*p.z()) );
+
+      T fz = M_PI*M_PI*(12*lambda*cos(2*M_PI*p.z())*sin(2*M_PI*p.y())*sin(2*M_PI*p.x())
+      + 8 * (3*cos(2*M_PI*p.z())-1)*sin(2*M_PI*p.y())*sin(2*M_PI*p.x())
+      - cos(M_PI*p.z())*sin(M_PI*(p.y()+p.x())) +
+      (1 + 3./(1+lambda))* sin(M_PI*p.x())*sin(M_PI*p.y())*sin(M_PI*p.z()) );
+
+      return result_type{fx,fy,fz};
+   };
+
+   auto solution = [](const point<T,3>& p) -> auto {
+      const T lambda =1.0;
+      T fx = sin(2*M_PI*p.y())*sin(2*M_PI*p.z())*(-1 + cos(2*M_PI*p.x()))
+      + (1./(1+lambda))* sin(M_PI*p.x())*sin(M_PI*p.y())*sin(M_PI*p.z());
+      T fy = sin(2*M_PI*p.z())*sin(2*M_PI*p.x())*(-1 + cos(2*M_PI*p.y()))
+      + (1./(1+lambda))* sin(M_PI*p.x())*sin(M_PI*p.y())*sin(M_PI*p.z());
+      T fz = sin(2*M_PI*p.x())*sin(2*M_PI*p.y())*(-1 + cos(2*M_PI*p.z()))
+      + (1./(1+lambda))* sin(M_PI*p.x())*sin(M_PI*p.y())*sin(M_PI*p.z());
+      return result_type{fx,fy,fz};
+   };
+
+
+   auto load_lin = [](const point<T,3>& p) -> result_type {
+      return result_type{0.0,0.0,0.0};
+   };
+
+   auto solution_lin = [](const point<T,3>& p) -> result_type {
+      const T scale = 1.0;
+      T fx = scale * p.x();
+      T fy = scale * p.y();
+      T fz = scale * p.z();
+      return result_type{fx,fy,fz};
+   };
+
+
+   CH_mixte_solver<Mesh, T, 3, Storage,  point<T, 3> > nl(msh, rp.degree);
+   nl.verbose(rp.verbose);
+
+   //auto info_offline = nl.compute_offline();
+
+   if(nl.verbose()){
+      std::cout << "Off_line computations: " << info_offline.time_offline << " sec"  << '\n';
+   }
+
+   nl.compute_initial_state();
+
+   if(nl.verbose()){
+      std::cout << "Solve the problem: " << '\n';
+   }
+
+   const size_t n_time_step = 1;
+
+   auto solve_info = nl.compute(load_lin, solution_lin, n_time_step);
+
+   if(nl.verbose()){
+      std::cout << "Total time to solve the problem: " << solve_info.time_solver << " sec" << '\n';
+   }
+
+   if(nl.test_convergence()){
+
+        std::cout << "l2 error: " << nl.compute_l2_error(solution_lin) << std::endl;
+
+        std::cout << "Post-processing: " << std::endl;
+        nl.plot_solution_at_gausspoint("sol_elas_3d.msh");
+        nl.plot_l2error_at_gausspoint("error_gp_3d_.msh", solution_lin);
+   }
+}
+
+
+int main(int argc, char **argv)
+{
+    using RealType = double;
+
+    char    *mesh_filename  = nullptr;
+    char    *plot_filename  = nullptr;
+    int     degree          = 1;
+    int     l               = 0;
+    int     elems_1d        = 8;
+
+    run_params rp;
+    rp.degree   = 1;
+    rp.l        = 0;
+    rp.verbose  = true;
+
+    int ch;
+
+    while ( (ch = getopt(argc, argv, "k:l:n:p:v")) != -1 )
+    {
+        switch(ch)
+        {
+            case 'k':
+                degree = atoi(optarg);
+                if (degree < 0)
+                {
+                    std::cout << "Degree must be positive. Falling back to 1." << std::endl;
+                    degree = 1;
+                }
+                rp.degree = degree;
+                break;
+
+            case 'l':
+                rp.l = atoi(optarg);
+                if (l < -1 or l > 1)
+                {
+                    std::cout << "l can be -1, 0 or 1. Falling back to 0." << std::endl;
+                    rp.l = 0;
+                }
+                break;
+
+            case 'n':
+                elems_1d = atoi(optarg);
+                if (elems_1d < 0)
+                {
+                    std::cout << "Num of elems must be positive. Falling back to 8." << std::endl;
+                    elems_1d = 8;
+                }
+                break;
+
+            case 'p':
+                plot_filename = optarg;
+                break;
+
+            case 'v':
+                rp.verbose = true;
+                break;
+
+            case 'h':
+            case '?':
+            default:
+                std::cout << "wrong arguments" << std::endl;
+                exit(1);
+        }
+    }
+
+    argc -= optind;
+    argv += optind;
+
+    if (argc == 0)
+    {
+        std::cout << "Mesh format: 1D uniform (Not avaible)" << std::endl;
+        auto msh = disk::load_uniform_1d_mesh<RealType>(0, 1, elems_1d);
+        //run_NL_elasticity_solver(msh, rp);
+        return 0;
+    }
+
+    mesh_filename = argv[0];
+
+    /* FVCA5 2D */
+    if (std::regex_match(mesh_filename, std::regex(".*\\.typ1$") ))
+    {
+        std::cout << "Guessed mesh format: FVCA5 2D" << std::endl;
+        auto msh = disk::load_fvca5_2d_mesh<RealType>(mesh_filename);
+        run_CH_mixte_solver(msh, rp);
+        return 0;
+    }
+
+    /* Netgen 2D */
+    if (std::regex_match(mesh_filename, std::regex(".*\\.mesh2d$") ))
+    {
+        std::cout << "Guessed mesh format: Netgen 2D" << std::endl;
+        auto msh = disk::load_netgen_2d_mesh<RealType>(mesh_filename);
+        run_CH_mixte_solver(msh, rp);
+        return 0;
+    }
+
+    /* DiSk++ cartesian 2D */
+    if (std::regex_match(mesh_filename, std::regex(".*\\.quad$") ))
+    {
+        std::cout << "Guessed mesh format: DiSk++ Cartesian 2D" << std::endl;
+        auto msh = disk::load_cartesian_2d_mesh<RealType>(mesh_filename);
+        run_CH_mixte_solver(msh, rp);
+        return 0;
+    }
+
+    /* Netgen 3D */
+    if (std::regex_match(mesh_filename, std::regex(".*\\.mesh$") ))
+    {
+        std::cout << "Guessed mesh format: Netgen 3D" << std::endl;
+        auto msh = disk::load_netgen_3d_mesh<RealType>(mesh_filename);
+        run_CH_mixte_solver(msh, rp);
+        return 0;
+    }
+
+    /* DiSk++ cartesian 3D */
+    if (std::regex_match(mesh_filename, std::regex(".*\\.hex$") ))
+    {
+        std::cout << "Guessed mesh format: DiSk++ Cartesian 3D" << std::endl;
+        auto msh = disk::load_cartesian_3d_mesh<RealType>(mesh_filename);
+        run_CH_mixte_solver(msh, rp);
+        return 0;
+    }
+
+}
---
+++ core/hho/hho_nl_diffusion.hpp
@@ -0,0 +1,235 @@
+/*
+ *       /\
+ *      /__\       Matteo Cicuttin (C) 2016 - matteo.cicuttin@enpc.fr
+ *     /_\/_\      École Nationale des Ponts et Chaussées - CERMICS
+ *    /\    /\
+ *   /__\  /__\    DISK++, a template library for DIscontinuous SKeletal
+ *  /_\/_\/_\/_\   methods.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * If you use this code for scientific publications, you are required to
+ * cite it.
+ */
+
+#pragma once
+
+#include "common/eigen.hpp"
+#include "bases/bases_utils.hpp"
+#include "bases/bases_ranges.hpp"
+#include "hho/hho.hpp"
+#include "timecounter.h"
+//#include "contrib/sol2/sol.hpp"
+
+//#define USE_BLAS
+#define FILL_COLMAJOR
+
+namespace disk {
+
+template<typename Mesh, //typename CellBasisType, typename CellQuadType,
+                        typename FaceBasisType, typename FaceQuadType>
+class assembler_nl_diffusion
+{
+    typedef Mesh                                mesh_type;
+    typedef typename mesh_type::scalar_type     scalar_type;
+    typedef typename mesh_type::cell            cell_type;
+    typedef typename mesh_type::face            face_type;
+
+    //typedef CellBasisType                       cell_basis_type;
+    //typedef CellQuadType                        cell_quadrature_type;
+    typedef FaceBasisType                       face_basis_type;
+    typedef FaceQuadType                        face_quadrature_type;
+
+    typedef dynamic_matrix<scalar_type>         matrix_type;
+
+
+    //cell_basis_type                             cell_basis;
+    //cell_quadrature_type                        cell_quadrature;
+
+    face_basis_type                             face_basis;
+    face_quadrature_type                        face_quadrature;
+
+    size_t                                      m_degree;
+
+    typedef Eigen::Triplet<scalar_type>         triplet_type;
+
+    std::vector<triplet_type>                   m_triplets;
+    size_t                                      m_num_unknowns;
+
+public:
+
+    typedef Eigen::SparseMatrix<scalar_type>    sparse_matrix_type;
+    typedef dynamic_vector<scalar_type>         vector_type;
+
+    sparse_matrix_type      matrix;
+    vector_type             rhs;
+
+    assembler_nl_diffusion()                 = delete;
+
+    assembler_nl_diffusion(const mesh_type& msh, size_t degree)
+        : m_degree(degree)
+    {
+        face_basis          = face_basis_type(m_degree);
+        face_quadrature     = face_quadrature_type(2*m_degree);
+
+        m_num_unknowns = face_basis.size() * (msh.faces_size() + msh.boundary_faces_size());
+        matrix = sparse_matrix_type(m_num_unknowns, m_num_unknowns);
+        rhs = vector_type::Zero(m_num_unknowns);
+    }
+
+    template<typename LocalContrib>
+    void
+    assemble(const mesh_type& msh, const cell_type& cl, const LocalContrib& lc)
+    {
+        auto fcs = faces(msh, cl);
+        std::vector<size_t> l2g(fcs.size() * face_basis.size());
+        for (size_t face_i = 0; face_i < fcs.size(); face_i++)
+        {
+            auto fc = fcs[face_i];
+            auto eid = find_element_id(msh.faces_begin(), msh.faces_end(), fc);
+            if (!eid.first)
+                throw std::invalid_argument("This is a bug: face not found");
+
+            auto face_id = eid.second;
+
+            auto face_offset = face_id * face_basis.size();
+
+            auto pos = face_i * face_basis.size();
+
+            for (size_t i = 0; i < face_basis.size(); i++)
+                l2g[pos+i] = face_offset+i;
+        }
+
+        assert(lc.first.rows() == lc.first.cols());
+        assert(lc.first.rows() == lc.second.size());
+        assert(lc.second.size() == l2g.size());
+
+        //std::cout << lc.second.size() << " " << l2g.size() << std::endl;
+
+#ifdef FILL_COLMAJOR
+        for (size_t j = 0; j < lc.first.cols(); j++)
+        {
+            for (size_t i = 0; i < lc.first.rows(); i++)
+                m_triplets.push_back( triplet_type( l2g.at(i), l2g.at(j), lc.first(i,j) ) );
+
+            rhs(l2g.at(j)) += lc.second(j);
+        }
+#else
+        for (size_t i = 0; i < lc.first.rows(); i++)
+        {
+            for (size_t j = 0; j < lc.first.cols(); j++)
+                m_triplets.push_back( triplet_type( l2g.at(i), l2g.at(j), lc.first(i,j) ) );
+
+            rhs(l2g.at(i)) += lc.second(i);
+        }
+#endif
+    }
+
+    template<typename Function>
+    void
+    impose_boundary_conditions(const mesh_type& msh, const Function& bc, const std::vector<vector_type>& sol_faces,
+                               const std::vector<vector_type>& sol_lagr)
+    {
+        size_t fbs = face_basis.size();
+        size_t face_i = 0;
+        size_t lagr_i = 0;
+        for (auto itor = msh.boundary_faces_begin(); itor != msh.boundary_faces_end(); itor++)
+        {
+            auto bfc = *itor;
+
+            auto eid = find_element_id(msh.faces_begin(), msh.faces_end(), bfc);
+            if (!eid.first)
+                throw std::invalid_argument("This is a bug: face not found");
+
+            auto face_id = eid.second;
+
+            auto face_offset = face_id * fbs;
+            auto face_offset_lagrange = (msh.faces_size() + face_i) * fbs;
+
+            auto fqd = face_quadrature.integrate(msh, bfc);
+
+            matrix_type MFF     = matrix_type::Zero(fbs, fbs);
+            vector_type rhs_f   = vector_type::Zero(fbs);
+
+            for (auto& qp : fqd)
+            {
+                auto f_phi = face_basis.eval_functions(msh, bfc, qp.point());
+                MFF += qp.weight() * f_phi * f_phi.transpose();
+                rhs_f += qp.weight() * f_phi * bc(qp.point());
+            }
+
+
+            rhs_f -= MFF * sol_faces.at(face_id);
+
+            vector_type rhs_l = MFF * sol_lagr.at(lagr_i);
+
+#ifdef FILL_COLMAJOR
+            for (size_t j = 0; j < MFF.cols(); j++)
+            {
+                for (size_t i = 0; i < MFF.rows(); i++)
+                {
+                    m_triplets.push_back( triplet_type(face_offset + i, face_offset_lagrange + j, MFF(i,j)) );
+                    m_triplets.push_back( triplet_type(face_offset_lagrange + j, face_offset + i, MFF(i,j)) );
+                }
+                rhs(face_offset_lagrange+j) = rhs_f(j);
+                rhs(face_offset+j) -= rhs_l(j);
+            }
+#else
+            for (size_t i = 0; i < MFF.rows(); i++)
+            {
+                for (size_t j = 0; j < MFF.cols(); j++)
+                {
+                    m_triplets.push_back( triplet_type(face_offset + i, face_offset_lagrange + j, MFF(i,j)) );
+                    m_triplets.push_back( triplet_type(face_offset_lagrange + j, face_offset + i, MFF(i,j)) );
+                }
+                rhs(face_offset_lagrange+i) = rhs_f(i);
+                rhs(face_offset+j) -= rhs_l(j);
+            }
+#endif
+
+            face_i++;
+            lagr_i++;
+        }
+    }
+
+    void
+    finalize()
+    {
+        matrix.setFromTriplets(m_triplets.begin(), m_triplets.end());
+        m_triplets.clear();
+    }
+
+    void
+    finalize(sparse_matrix_type& mat, vector_type& vec)
+    {
+        mat = sparse_matrix_type(m_num_unknowns, m_num_unknowns);
+        mat.setFromTriplets(m_triplets.begin(), m_triplets.end());
+        m_triplets.clear();
+        vec = rhs;
+    }
+};
+
+
+template<typename CellBasisType, typename CellQuadType, typename Mesh,
+         typename Function>
+dynamic_vector<typename Mesh::scalar_type>
+compute_rhs_diffusion(const Mesh& msh, const typename Mesh::cell& cl,
+            const Function& f, const size_t degree,
+            const dynamic_matrix<typename Mesh::scalar_type>& mat_loc,
+            const dynamic_vector<typename Mesh::scalar_type>& solution)
+{
+    typedef dynamic_vector<typename Mesh::scalar_type> vector_type;
+
+    vector_type ret = mat_loc * solution;
+
+    vector_type rhs_ext = compute_rhs<CellBasisType, CellQuadType>(msh, cl, f, degree);
+
+    ret.block(0,0, rhs_ext.rows(), rhs_ext.cols()) -= rhs_ext;
+
+    return ret;
+}
+
+
+} // namespace disk
