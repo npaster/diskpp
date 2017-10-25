5e77f6b0b7d4975dcd4603ee622f0c29943b4214
npaster <nicolas.pignet@orange.fr>
2017-10-25 18:47:27 +0200

    ajout assembler sans condensation statique cas non linéaire

--- core/hho/hho_nl_vector.hpp
+++ core/hho/hho_nl_vector.hpp
@@ -704,7 +704,7 @@ namespace disk {
       {
          const auto fcs = faces(msh, cl);
          const size_t face_basis_size = m_bqd.face_basis.size();
-         
+
          std::vector<size_t> l2g(fcs.size() * face_basis_size);
          for (size_t face_i = 0; face_i < fcs.size(); face_i++)
          {
@@ -1005,6 +1005,348 @@ namespace disk {
                const size_t lagr_basis_size = nb_lag * lagr_size;
                const size_t lag_pos = boundary_conditions.begin_lag_conditions_faceI(face_dir);
                const size_t face_offset_lagrange = msh.faces_size()  * face_basis_size + lag_pos * lagr_size;
+
+               const auto fqd = m_bqd.face_quadrature.integrate(msh, bfc);
+
+               matrix_type MFF     = matrix_type::Zero(face_basis_size, face_basis_size);
+               vector_type rhs_bc   = vector_type::Zero(face_basis_size);
+               vector_type rhs_l   = vector_type::Zero(face_basis_size);
+               for (auto& qp : fqd)
+               {
+                  const auto f_phi = m_bqd.face_basis.eval_functions(msh, bfc, qp.point());
+                  assert(f_phi.size() == face_basis_size);
+                  for(size_t i = 0; i < face_basis_size; i++)
+                     for(size_t j = i; j < face_basis_size; j++)
+                        MFF(i,j) += qp.weight() * mm_prod(f_phi[i], f_phi[j]);
+
+
+                     for(size_t i = 0; i < face_basis_size; i++)
+                        rhs_bc(i) += qp.weight() * mm_prod( f_phi[i],  bc(qp.point()));
+               }
+
+               //lower part
+               for(size_t i = 1; i < face_basis_size; i++)
+                  for(size_t j = 0; j < i; j++)
+                     MFF(i,j) = MFF(j,i) ;
+
+               // impose displacement
+               const vector_type rhs_fc = rhs_bc - MFF * sol_faces.at(face_id);
+               const vector_type rhs_f = - MFF * sol_faces.at(face_id);
+
+               vector_type rhs_f2;
+               matrix_type MFF2;
+
+               bool dirichlet_standart = true;
+               switch ( boundary_conditions.boundary_type(face_i)) {
+                  case CLAMPED:
+                  {
+                     dirichlet_standart = false;
+                     rhs_f2 = rhs_f;
+                     MFF2 = MFF;
+                     rhs_l = MFF * sol_lagr.at(face_dir);
+                     break;
+                  }
+                  case DX:
+                  {
+                     dirichlet_standart = false;
+                     rhs_f2.resize(lagr_basis_size, 1);
+                     MFF2.resize(face_basis_size, lagr_basis_size);
+
+                     assert(rhs_f2.rows() == lagr_basis_size);
+                     assert(MFF2.rows() == face_basis_size);
+                     assert(MFF2.cols() == lagr_basis_size);
+
+                     size_t ind(0);
+                     for(size_t i = 0; i < face_basis_size; i += DIM){
+                        MFF2.col(ind) = MFF.col(i);
+                        rhs_f2(ind) = rhs_f(i);
+                        ind++;
+                     }
+
+                     assert(ind == lagr_basis_size);
+
+                     vector_type rhs_l2 = MFF2 * sol_lagr.at(face_dir);
+                     ind = 0;
+                     for(size_t i = 0; i < face_basis_size; i += DIM){
+                        rhs_l(i) = rhs_l2(ind);
+                        ind++;
+                     }
+
+                     assert(ind == lagr_basis_size);
+
+                     break;
+                  }
+                  //
+                  //                   case DY:
+                  //                      nb_lag_conditions -= DIM - 1;
+                  //                      break;
+                  //                   case DZ:
+                  //                      if( DIM != 3){
+                  //                         std::cout << "Invalid condition for face:" << face_id << std::endl;
+                  //                         throw std::invalid_argument(" ONLY DIM = 3 for this Dirichlet Conditions");
+                  //                      }
+                  //                      else
+                  //                         nb_lag_conditions -= 2;
+                  //                      break;
+                  //                   case DXDY:
+                  //                      if( DIM != 3){
+                  //                         std::cout << "Invalid condition for face:" << face_id << std::endl;
+                  //                         throw std::invalid_argument(" ONLY DIM = 3 for this Dirichlet Conditions");
+                  //                      }
+                  //                      else
+                  //                         nb_lag_conditions -= 1;
+                  //                      break;
+                  //                   case DXDZ:
+                  //                      if( DIM != 3){
+                  //                         std::cout << "Invalid condition for face:" << face_id << std::endl;
+                  //                         throw std::invalid_argument(" ONLY DIM = 3 for this Dirichlet Conditions");
+                  //                      }
+                  //                      else
+                  //                         nb_lag_conditions -= 1;
+                  //                      break;
+                  //                   case DYDZ:
+                  //                      if( DIM != 3){
+                  //                         std::cout << "Invalid condition for face:" << face_id << std::endl;
+                  //                         throw std::invalid_argument(" ONLY DIM = 3 for this Dirichlet Conditions");
+                  //                      }
+                  //                      else
+                  //                         nb_lag_conditions -= 1;
+                  //                      break;
+                  case OTHER: break;
+                  default:
+                  std::cout << "Unknown Dirichlet Conditions: we do nothing" << std::endl;
+                  break;
+
+               }
+
+               if(dirichlet_standart){
+                  rhs_f2 = rhs_fc;
+                  MFF2 = MFF;
+                  rhs_l = MFF * sol_lagr.at(face_dir);
+               }
+
+               assert(MFF2.rows() == face_basis_size);
+
+               #ifdef FILL_COLMAJOR
+               for (size_t j = 0; j < MFF2.cols(); j++)
+               {
+                  for (size_t i = 0; i < MFF2.rows(); i++)
+                  {
+                     m_triplets.push_back( triplet_type(face_offset + i, face_offset_lagrange + j, MFF2(i,j)) );
+                     m_triplets.push_back( triplet_type(face_offset_lagrange + j, face_offset + i, MFF2(i,j)) );
+                  }
+                  rhs(face_offset_lagrange+j) = rhs_f2(j);
+
+               }
+
+               for (size_t i = 0; i < face_basis_size; i++)
+                  rhs(face_offset+i) -= rhs_l(i);
+
+               #else
+               for (size_t i = 0; i < MFF2.rows(); i++)
+               {
+                  for (size_t j = 0; j < MFF2.cols(); j++)
+                  {
+                     m_triplets.push_back( triplet_type(face_offset + i, face_offset_lagrange + j, MFF2(i,j)) );
+                     m_triplets.push_back( triplet_type(face_offset_lagrange + j, face_offset + i, MFF2(i,j)) );
+                  }
+                  rhs(face_offset_lagrange+i) = rhs_f2(i);
+                  rhs(face_offset+j) -= rhs_l(j);
+               }
+               #endif
+               face_dir++;
+            }
+            else{
+
+               if(boundary_conditions.boundary_type(face_i) == NEUMANN){
+                  vector_type TF   = vector_type::Zero(face_basis_size);
+
+                  const auto face_quadpoints = m_bqd.face_quadrature.integrate(msh, bfc);
+                  for (auto& qp : face_quadpoints)
+                  {
+                     auto fphi = m_bqd.face_basis.eval_functions(msh, bfc, qp.point());
+                     assert(fphi.size() == face_basis_size);
+
+                     for(size_t i=0; i < face_basis_size; i++)
+                        TF(i) += qp.weight() * mm_prod(g(qp.point()) , fphi[i]);
+                  }
+
+                  for (size_t i = 0; i < TF.rows(); i++)
+                  {
+                     rhs(face_offset+i) += TF(i);
+                  }
+               }
+            }
+            face_i++;
+         }
+      }
+
+      void
+      finalize()
+      {
+         matrix.setFromTriplets(m_triplets.begin(), m_triplets.end());
+         m_triplets.clear();
+      }
+
+      void
+      finalize(sparse_matrix_type& mat, vector_type& vec)
+      {
+         mat = sparse_matrix_type(m_num_unknowns, m_num_unknowns);
+         mat.setFromTriplets(m_triplets.begin(), m_triplets.end());
+         m_triplets.clear();
+         vec = rhs;
+      }
+   };
+
+
+
+
+
+   template<typename BQData>
+   class assembler_full_nl_vector_bq
+   {
+      typedef typename BQData::mesh_type          mesh_type;
+      typedef typename mesh_type::scalar_type     scalar_type;
+      typedef typename mesh_type::cell            cell_type;
+
+      typedef dynamic_matrix<scalar_type>         matrix_type;
+
+
+      typedef Eigen::Triplet<scalar_type>         triplet_type;
+
+      std::vector<triplet_type>                   m_triplets;
+      size_t                                      m_num_unknowns;
+
+      const BQData&                               m_bqd;
+
+   public:
+
+      typedef Eigen::SparseMatrix<scalar_type>    sparse_matrix_type;
+      typedef dynamic_vector<scalar_type>         vector_type;
+
+      sparse_matrix_type      matrix;
+      vector_type             rhs;
+
+      assembler_full_nl_vector_bq()                 = delete;
+
+      assembler_full_nl_vector_bq(const mesh_type& msh, const BQData& bqd)
+      : m_bqd(bqd)
+      {
+         const size_t cells_unknows = m_bqd.cell_basis.size() * msh.cells_size();
+         const size_t faces_unknows = m_bqd.face_basis.size() * (msh.faces_size() + msh.boundary_faces_size());
+         m_num_unknowns = cells_unknows + faces_unknows;
+         matrix = sparse_matrix_type(m_num_unknowns, m_num_unknowns);
+         rhs = vector_type::Zero(m_num_unknowns);
+      }
+
+      assembler_full_nl_vector_bq(const mesh_type& msh, const BQData& bqd, const BoundaryConditions& boundary_conditions)
+      : m_bqd(bqd)
+      {
+         const size_t cells_unknows = m_bqd.cell_basis.size() * msh.cells_size();
+         const size_t faces_unknows = m_bqd.face_basis.size() * (msh.faces_size() + msh.boundary_faces_size());
+         const size_t lag_unknows = boundary_conditions.nb_lag() * m_bqd.face_basis.size()/msh.dimension;
+         m_num_unknowns = cells_unknows + faces_unknows + lag_unknows;
+         matrix = sparse_matrix_type(m_num_unknowns, m_num_unknowns);
+         rhs = vector_type::Zero(m_num_unknowns);
+      }
+
+      template<typename LocalContrib>
+      void
+      assemble(const mesh_type& msh, const cell_type& cl, const LocalContrib& lc)
+      {
+         //assemble cell Parameters
+         const size_t cell_basis_size = m_bqd.cell_basis.size();
+         const size_t face_basis_size = m_bqd.face_basis.size();
+         const auto fcs = faces(msh, cl);
+         const size_t total_dof = cell_basis_size + fcs.size() * face_basis_size;
+
+         std::vector<size_t> l2g(total_dof);
+
+         const auto cid = find_element_id(msh.cells_begin(), msh.cells_end(), cl);
+         if (!cid.first)
+            throw std::invalid_argument("This is a bug: cell not found");
+
+         const auto cell_id = cid.second;
+         const auto cell_offset = cell_id * cell_basis_size;
+
+         for (size_t i = 0; i < cell_basis_size; i++) {
+            l2g[i] = cell_offset + i;
+         }
+
+         const size_t cells_offset = msh.cells_size() * cell_basis_size;
+         for (size_t face_i = 0; face_i < fcs.size(); face_i++)
+         {
+            const auto fc = fcs[face_i];
+            const auto eid = find_element_id(msh.faces_begin(), msh.faces_end(), fc);
+            if (!eid.first)
+               throw std::invalid_argument("This is a bug: face not found");
+
+            const auto face_id = eid.second;
+            const auto face_offset = cells_offset + face_id * face_basis_size;
+            const auto pos = cell_basis_size + face_i * face_basis_size;
+
+            for (size_t i = 0; i < face_basis_size; i++)
+               l2g[pos+i] = face_offset+i;
+         }
+
+         assert(lc.first.rows() == lc.first.cols());
+         assert(lc.first.rows() == lc.second.size());
+         assert(lc.second.size() == l2g.size());
+
+         //std::cout << lc.second.size() << " " << l2g.size() << std::endl;
+
+         #ifdef FILL_COLMAJOR
+         for (size_t j = 0; j < lc.first.cols(); j++)
+         {
+            for (size_t i = 0; i < lc.first.rows(); i++)
+               m_triplets.push_back( triplet_type( l2g.at(i), l2g.at(j), lc.first(i,j) ) );
+
+            rhs(l2g.at(j)) += lc.second(j);
+         }
+         #else
+         for (size_t i = 0; i < lc.first.rows(); i++)
+         {
+            for (size_t j = 0; j < lc.first.cols(); j++)
+               m_triplets.push_back( triplet_type( l2g.at(i), l2g.at(j), lc.first(i,j) ) );
+
+            rhs(l2g.at(i)) += lc.second(i);
+         }
+         #endif
+      }
+
+
+      template<typename Function, typename NeumannFunction>
+      void
+      impose_boundary_conditions(const mesh_type& msh, const Function& bc,
+                                 const NeumannFunction& g,
+                                 const std::vector<vector_type>& sol_faces,
+                                 const std::vector<vector_type>& sol_lagr,
+                                 const BoundaryConditions& boundary_conditions)
+      {
+         const size_t DIM = msh.dimension;
+         const size_t face_basis_size = m_bqd.face_basis.size();
+         const size_t lagr_size = face_basis_size / DIM;
+         size_t face_i = 0;
+         size_t face_dir(0);
+         const size_t cells_offset = msh.cells_size() * m_bqd.cell_basis.size();
+
+         for (auto itor = msh.boundary_faces_begin(); itor != msh.boundary_faces_end(); itor++)
+         {
+            auto bfc = *itor;
+
+            const auto eid = find_element_id(msh.faces_begin(), msh.faces_end(), bfc);
+            if (!eid.first)
+               throw std::invalid_argument("This is a bug: face not found");
+
+            const auto face_id = eid.second;
+            const size_t face_offset = cells_offset + face_id * face_basis_size;
+
+            //std::cout << "face "  << face_i << " "<< boundary_conditions.is_boundary_dirichlet(face_i)   << '\n';
+            if(boundary_conditions.is_boundary_dirichlet(face_i)){
+               const size_t nb_lag = boundary_conditions.nb_lag_conditions_faceI(face_dir);
+               const size_t lagr_basis_size = nb_lag * lagr_size;
+               const size_t lag_pos = boundary_conditions.begin_lag_conditions_faceI(face_dir);
+               const size_t face_offset_lagrange = cells_offset + msh.faces_size()  * face_basis_size + lag_pos * lagr_size;
 
                const auto fqd = m_bqd.face_quadrature.integrate(msh, bfc);
 
