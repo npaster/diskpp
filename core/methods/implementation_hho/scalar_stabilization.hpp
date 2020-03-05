d429da79d6d2acd931ce26d0399c4ca778914be2
npaster <nicolas.pignet@orange.fr>
2020-03-05 22:05:43 +0100

    add hho stabilization and adjoint operator for k+1/k

--- core/methods/implementation_hho/scalar_stabilization.hpp
+++ core/methods/implementation_hho/scalar_stabilization.hpp
@@ -611,64 +611,187 @@ make_scalar_hho_stabilization(const Mesh&                                       
  */
 template<typename Mesh>
 dynamic_matrix<typename Mesh::coordinate_type>
+make_scalar_hho_stabilization_diff(const Mesh&                                           msh,
+                                   const typename Mesh::cell_type&                       cl,
+                                   const dynamic_matrix<typename Mesh::coordinate_type>& reconstruction,
+                                   const CellDegreeInfo<Mesh>&                           cell_infos)
+{
+    using T = typename Mesh::coordinate_type;
+    typedef Matrix<T, Dynamic, Dynamic> matrix_type;
+
+    const auto recdeg = cell_infos.reconstruction_degree();
+    const auto celdeg = cell_infos.cell_degree();
+
+    const auto rbs = scalar_basis_size(recdeg, Mesh::dimension);
+    const auto cbs = scalar_basis_size(celdeg, Mesh::dimension);
+
+    const auto faces_infos = cell_infos.facesDegreeInfo();
+
+    const auto num_diff_dofs  = scalar_diff_dofs(msh, cell_infos);
+    const auto num_faces_dofs = scalar_faces_dofs(msh, faces_infos);
+
+    matrix_type data = matrix_type::Zero(num_diff_dofs, num_diff_dofs);
+
+    const auto cb = make_scalar_monomial_basis(msh, cl, recdeg);
+
+    const matrix_type mass_mat = make_mass_matrix(msh, cl, cb);
+
+    // Build \pi_F^k (v_F - P_T^K v) equations (21) and (22)
+
+    // Step 1: compute \pi_T^k p_T^k v (third term).
+    const matrix_type M1    = mass_mat.block(0, 0, cbs, cbs);
+    const matrix_type M2    = mass_mat.block(0, 1, cbs, rbs - 1);
+    matrix_type       proj1 = M1.ldlt().solve(M2 * reconstruction);
+
+    assert(M2.cols() == reconstruction.rows());
+
+    const auto fcs         = faces(msh, cl);
+    size_t     offset      = 0;
+    size_t     offset_diff = 0;
+    for (size_t i = 0; i < fcs.size(); i++)
+    {
+        const auto fdi = faces_infos[i];
+
+        if (fdi.hasUnknowns())
+        {
+            const auto fc     = fcs[i];
+            const auto facdeg = fdi.degree();
+            const auto hF     = diameter(msh, fc);
+
+            const auto diff_deg = std::max(celdeg, facdeg);
+            const auto fb       = make_scalar_monomial_basis(msh, fc, facdeg);
+            const auto db       = make_scalar_monomial_basis(msh, fc, diff_deg);
+
+            const auto fbs = scalar_basis_size(facdeg, Mesh::dimension - 1);
+            const auto dbs = scalar_basis_size(diff_deg, Mesh::dimension - 1);
+
+            // PikF(theta)
+            matrix_type face_mass = matrix_type::Zero(fbs, fbs);
+            matrix_type trace     = matrix_type::Zero(fbs, dbs);
+
+            const auto qps = integrate(msh, fc, facdeg + diff_deg);
+            for (auto& qp : qps)
+            {
+                const auto d_phi = db.eval_functions(qp.point());
+                const auto f_phi = fb.eval_functions(qp.point());
+
+                face_mass += priv::outer_product(priv::inner_product(qp.weight(), f_phi), f_phi);
+                trace += priv::outer_product(priv::inner_product(qp.weight(), f_phi), d_phi);
+            }
+
+            matrix_type oper = matrix_type::Zero(fbs, num_diff_dofs);
+            matrix_type tr   = matrix_type::Zero(fbs, num_diff_dofs);
+
+            LDLT<matrix_type> piKF;
+            piKF.compute(face_mass);
+
+            tr.block(0, offset_diff, fbs, dbs) = trace;
+
+            oper.block(0, offset_diff, fbs, dbs) = piKF.solve(trace);
+
+            data += oper.transpose() * face_mass * oper / hF;
+
+            offset += fbs;
+            offset_diff += dbs;
+        }
+    }
+
+    return data;
+
+    // // Step 3: project on faces (eqn. 21)
+    // size_t offset = cbs;
+    // for (size_t face_i = 0; face_i < fcs.size(); face_i++)
+    // {
+    //     const auto fdi = faces_infos[face_i];
+
+    //     if (fdi.hasUnknowns())
+    //     {
+    //         const auto fc = fcs[face_i];
+    //         const auto hf = diameter(msh, fc);
+
+    //         const auto facdeg = fdi.degree();
+    //         const auto fb     = make_scalar_monomial_basis(msh, fc, facdeg);
+    //         const auto fbs    = scalar_basis_size(facdeg, Mesh::dimension - 1);
+
+    //         matrix_type face_mass_matrix  = make_mass_matrix(msh, fc, fb);
+    //         matrix_type face_trace_matrix = matrix_type::Zero(fbs, rbs);
+
+    //         const auto face_quadpoints = integrate(msh, fc, recdeg + facdeg);
+    //         for (auto& qp : face_quadpoints)
+    //         {
+    //             const auto f_phi = fb.eval_functions(qp.point());
+    //             const auto c_phi = cb.eval_functions(qp.point());
+    //             face_trace_matrix += priv::outer_product(priv::inner_product(qp.weight(), f_phi), c_phi);
+    //         }
+
+    //         LLT<matrix_type> piKF;
+    //         piKF.compute(face_mass_matrix);
+
+    //         // Step 3a: \pi_F^k( v_F - p_T^k v )
+    //         const matrix_type MR1 = face_trace_matrix.block(0, 1, fbs, rbs - 1);
+
+    //         matrix_type proj2 = piKF.solve(MR1 * reconstruction);
+    //         proj2.block(0, offset, fbs, fbs) -= matrix_type::Identity(fbs, fbs);
+
+    //         // Step 3b: \pi_F^k( v_T - \pi_T^k p_T^k v )
+    //         const matrix_type MR2   = face_trace_matrix.block(0, 0, fbs, cbs);
+    //         const matrix_type proj3 = piKF.solve(MR2 * proj1);
+    //         const matrix_type BRF   = proj2 + proj3;
+
+    //         data += BRF.transpose() * face_mass_matrix * BRF / hf;
+
+    //         offset += fbs;
+    //     }
+    // }
+}
+
+/**
+ * @brief compute the stabilization term \f$\sum_{F \in F_T} 1/h_F(u_F - u_T + \Pi^k_T R^{k+1}_T(\hat{u}_T) -
+ * R^{k+1}_T(\hat{u}_T), v_F - v_T + \Pi^k_T R^{k+1}_T(\hat{v}_T) - R^{k+1}_T(\hat{v}_T))_F \f$ for scalar HHO
+ * unknowns
+ *
+ * @tparam Mesh type of the mesh
+ * @param msh mesh
+ * @param cl cell
+ * @param reconstruction reconstruction operator \f$ R^{k+1}_T \f$
+ * @param di hho degree information
+ * @return dynamic_matrix<typename Mesh::coordinate_type> return the stabilization term
+ */
+template<typename Mesh>
+dynamic_matrix<typename Mesh::coordinate_type>
+make_scalar_hho_stabilization_diff(const Mesh&                                           msh,
+                                   const typename Mesh::cell_type&                       cl,
+                                   const dynamic_matrix<typename Mesh::coordinate_type>& reconstruction,
+                                   const hho_degree_info&                                di)
+{
+    const CellDegreeInfo<Mesh> cell_infos(msh, cl, di.cell_degree(), di.face_degree(), di.grad_degree());
+
+    return make_scalar_hho_stabilization_diff(msh, cl, reconstruction, cell_infos);
+}
+
+/**
+ * @brief compute the stabilization term \f$\sum_{F \in F_T} 1/h_F(u_F - u_T + \Pi^k_T R^{k+1}_T(\hat{u}_T) -
+ * R^{k+1}_T(\hat{u}_T), v_F - v_T + \Pi^k_T R^{k+1}_T(\hat{v}_T) - R^{k+1}_T(\hat{v}_T))_F \f$ for scalar HHO
+ * unknowns
+ *
+ * @tparam Mesh type of the mesh
+ * @param msh mesh
+ * @param cl cell
+ * @param reconstruction reconstruction operator \f$ R^{k+1}_T \f$
+ * @param cell_infos cell degree information
+ * @return dynamic_matrix<typename Mesh::coordinate_type> return the stabilization term
+ */
+template<typename Mesh>
+dynamic_matrix<typename Mesh::coordinate_type>
 make_scalar_hho_stabilization_adjoint(const Mesh&                                           msh,
                                       const typename Mesh::cell_type&                       cl,
                                       const dynamic_matrix<typename Mesh::coordinate_type>& reconstruction,
                                       const CellDegreeInfo<Mesh>&                           cell_infos)
 {
-    using T = typename Mesh::coordinate_type;
-    typedef Matrix<T, Dynamic, Dynamic> matrix_type;
+    const auto stab = make_scalar_hho_stabilization_diff(msh, cl, reconstruction, cell_infos);
 
-    const matrix_type stab = make_scalar_hho_stabilization(msh, cl, reconstruction, cell_infos);
-
-    const auto celdeg = cell_infos.cell_degree();
-
-    const auto cbs = scalar_basis_size(celdeg, Mesh::dimension);
-
-    const auto cb = make_scalar_monomial_basis(msh, cl, celdeg);
-
-    const auto fcs            = faces(msh, cl);
-    const auto faces_infos    = cell_infos.facesDegreeInfo();
-    const auto num_faces_dofs = scalar_faces_dofs(msh, faces_infos);
-
-    matrix_type matA = matrix_type::Zero(num_faces_dofs, cbs + num_faces_dofs);
-
-    // Step 3: project on faces (eqn. 21)
-    size_t offset = cbs;
-    size_t row    = 0;
-    for (size_t face_i = 0; face_i < fcs.size(); face_i++)
-    {
-        const auto fdi = faces_infos[face_i];
-
-        if (fdi.hasUnknowns())
-        {
-            const auto fc = fcs[face_i];
-            const auto hf = diameter(msh, fc);
-
-            const auto facdeg = fdi.degree();
-            const auto fb     = make_scalar_monomial_basis(msh, fc, facdeg);
-            const auto fbs    = scalar_basis_size(facdeg, Mesh::dimension - 1);
-
-            matrix_type face_mass_matrix  = make_mass_matrix(msh, fc, fb);
-            matrix_type face_trace_matrix = matrix_type::Zero(fbs, cbs);
-
-            const auto face_quadpoints = integrate(msh, fc, celdeg + facdeg);
-            for (auto& qp : face_quadpoints)
-            {
-                const auto f_phi = fb.eval_functions(qp.point());
-                const auto c_phi = cb.eval_functions(qp.point());
-                face_trace_matrix += priv::outer_product(priv::inner_product(qp.weight(), f_phi), c_phi);
-            }
-
-            matA.block(row, offset, fbs, fbs) += face_mass_matrix / hf;
-            matA.block(row, 0, fbs, cbs) -= face_trace_matrix / hf;
-
-            offset += fbs;
-            row += fbs;
-        }
-    }
-
-    return (matA.transpose()).fullPivHouseholderQr().solve(stab);
+    const auto adjoint = make_scalar_stabilization_adjoint(msh, cl, cell_infos, stab);
+    return adjoint * make_scalar_hho_difference(msh, cl, cell_infos);
 }
 
 /**
