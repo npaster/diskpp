ef0bc1848451080870bb6006a9961cbe0af7e180
npaster <nicolas.pignet@orange.fr>
2021-06-22 18:17:42 +0200

    Try an adaptative stabilization

--- core/mechanics/NewtonSolver/NewtonSolver.hpp
+++ core/mechanics/NewtonSolver/NewtonSolver.hpp
@@ -1108,7 +1108,7 @@ class NewtonSolver
         {
 
             std::array<double, 3> coor = init_coor(barycenter(m_msh, cl));
-            double                beta = m_stab_manager.getValue(m_msh, cl);
+            double                beta = m_stab_manager.getValue(m_msh, cl) ;
             std::vector<double>   tens(1, beta);
 
             // Add GP
